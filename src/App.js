882808b3fd95cbd50f6df3379a572e13770884da
Logan Cooper <ldtcoop@gmail.com>
2017-11-21 15:24:26 -0800

    Get rid of raw data json

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Chart from './chart.js';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <h1>CO2 Emissions by Region</h1>
+        <Chart />
       </div>
     );
   }
--- src/chart.js
+++ src/chart.js
@@ -1,7 +1,23 @@
 import React, {Component} from 'react';
 import data from '/data.js';
 import {
+  AreaChart,
   CartesianGrid,
   XAxis,
   YAxis
 } from 'recharts';
+
+class Chart extends Component {
+  constructor(props) {
+    super(props);
+    this.data = this.props.data;
+  }
+
+  render() {
+    return(
+      <AreaChart>
+        <CartesianGrid strokeDashArray = "3 3"/>
+      </AreaChart>
+    );
+  }
+}
