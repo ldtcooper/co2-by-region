f0ecb860e279a3c0ef4724e638551bb54d3d5875
Logan Cooper <ldtcoop@gmail.com>
2017-11-21 15:44:04 -0800

    Add template to parser

---
+++ datasets/parser.py
@@ -0,0 +1,26 @@
+# needs to parse into format:
+# {
+#     2000: {
+#         year: 2000,
+#         Africa: 10,
+#         Asia: 20,
+#         North America: 30
+#     },
+#     2001: {
+#         year: 2001,
+#         Africa: 11,
+#         Asia: 22,
+#         North America: 33
+#     }
+# }
+
+import csv
+
+template_dict = {}
+
+with open('raw_csv_data.csv', 'rt') as f:
+    reader = csv.reader(f)
+    for row in reader:
+        if row[0] == "Country Name":
+            for el in row[0]:
+                template_dict[]
