876a466bfd332f576d186ed5a74fd913afd7cfeb
Logan Cooper <ldtcoop@gmail.com>
2017-11-22 16:32:18 -0800

    Add region names

--- src/chart.js
+++ src/chart.js
@@ -14,11 +14,51 @@ class Chart extends Component {
     this.data = this.props.data;
   }
 
+  propsList() {
+      const props = [
+        {
+          region: "Sub-Saharan Africa",
+          stroke: ,
+          fill:
+        },
+        {
+          region: "Latin America & Caribbean",
+          stroke: ,
+          fill:
+        },
+        {
+          region: "East Asia & Pacific",
+          stroke: ,
+          fill:
+        },
+        {
+          region: "Europe & Central Asia",
+          stroke: ,
+          fill:
+        },
+        {
+          region: "Middle East & North Africa",
+          stroke: ,
+          fill:
+        },
+        {
+          region: "North America",
+          stroke: ,
+          fill:
+        },
+        {
+          region: "South Asia",
+          stroke: ,
+          fill:
+        }
+      ];
+    }
+
   render() {
     return(
       //<ResponsiveContainer>
         <AreaChart width={1200} height={600} data={this.data}>
-          <XAxis dataKey="year">
+          <XAxis dataKey="year" scale="time">
             <Label value="Year" offset={0} position="insideBottom" />
           </XAxis>
           <YAxis label={{ value: 'CO2 Emissions\n(Metric Tons per Capita)', angle: -90, position: 'insideLeft' }}/>
