98ecb8e11882fa9f3886fcad3cb534af064bbf87
Logan Cooper <ldtcoop@gmail.com>
2017-11-24 15:29:22 -0800

    Convert chart into functional component

--- src/chart.js
+++ src/chart.js
@@ -10,32 +10,27 @@ import {
 } from 'recharts';
 import AreaComponent from './area.js';
 
-class Chart extends Component {
-  constructor(props) {
-    super(props);
-  }
+const Chart = (props) => {
+  return(
+    //<ResponsiveContainer>
+      <AreaChart width={1200} height={600} data={props.data}
+        margin={{top: 10, right: 30, left: 0, bottom: 0}}>
+        <XAxis dataKey="year" scale="time" minTickGap={2}>
+          <Label value="Year" offset={0} position="insideBottom" />
+        </XAxis>
+        <YAxis />
+        <CartesianGrid strokeDashArray = "3 3"/>
+        <Area type="monotone" dataKey="South Asia" stroke="#381A1C" fill="#381A1C" stackId="1"/>
+        <Area type="monotone" dataKey="Sub-Saharan Africa" stroke="#563547" fill="#563547" stackId="1"/>
+        <Area type="monotone" dataKey="Latin America & Caribbean" stroke="#5C5C78" fill="#5C5C78" stackId="1"/>
+        <Area type="monotone" dataKey="East Asia & Pacific" stroke="#46889B" fill="#46889B" stackId="1"/>
+        <Area type="monotone" dataKey="Middle East & North Africa" stroke="#3DB3A1" fill="#3DB3A1" stackId="1"/>
+        <Area type="monotone" dataKey="Europe & Central Asia" stroke="#7FD98D" fill="#7FD98D" stackId="1"/>
+        <Area type="monotone" dataKey="North America" stroke="#E2F573" fill="#E2F573" stackId="1"/>
+      </AreaChart>
+    //</ResponsiveContainer>
+  );
+};
 
-  render() {
-    return(
-      //<ResponsiveContainer>
-        <AreaChart width={1200} height={600} data={this.props.data}
-          margin={{top: 10, right: 30, left: 0, bottom: 0}}>
-          <XAxis dataKey="year" scale="time" minTickGap={2}>
-            <Label value="Year" offset={0} position="insideBottom" />
-          </XAxis>
-          <YAxis />
-          <CartesianGrid strokeDashArray = "3 3"/>
-          <Area type="monotone" dataKey="South Asia" stroke="#381A1C" fill="#381A1C" stackId="1"/>
-          <Area type="monotone" dataKey="Sub-Saharan Africa" stroke="#563547" fill="#563547" stackId="1"/>
-          <Area type="monotone" dataKey="Latin America & Caribbean" stroke="#5C5C78" fill="#5C5C78" stackId="1"/>
-          <Area type="monotone" dataKey="East Asia & Pacific" stroke="#46889B" fill="#46889B" stackId="1"/>
-          <Area type="monotone" dataKey="Middle East & North Africa" stroke="#3DB3A1" fill="#3DB3A1" stackId="1"/>
-          <Area type="monotone" dataKey="Europe & Central Asia" stroke="#7FD98D" fill="#7FD98D" stackId="1"/>
-          <Area type="monotone" dataKey="North America" stroke="#E2F573" fill="#E2F573" stackId="1"/>
-        </AreaChart>
-      //</ResponsiveContainer>
-    );
-  }
-}
 
 export default Chart;
